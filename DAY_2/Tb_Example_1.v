e134758345a39256e135c9493b0d186466c3faaf
hemanthnarava2003-netizen <hemanthnarava2003@gmail.com>
2025-10-29 20:13:59 +0530

    Update Tb_Example_1.v

--- DAY_2/Tb_Example_1.v
+++ DAY_2/Tb_Example_1.v
@@ -1,1 +1,20 @@
+module tb_param_palocalparam;
+    reg  [3:0] a, b;
+    wire [3:0] sum;
+    param_localparam #(.WIDTH(4)) uut1 (
+        .a(a),
+        .b(b),
+        .sum(sum)
+    );
 
+    initial begin     
+        a = 8'b0011;   
+        b = 8'b0101;   
+        #10;
+
+        $display("a = %b, b = %b, sum = %b", a, b, sum);
+
+        #10 $finish;
+    end
+
+endmodule
