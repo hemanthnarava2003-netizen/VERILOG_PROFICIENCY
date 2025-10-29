48563fda1fb73b012df12f6d0bda1063e3a44bc3
hemanthnarava2003-netizen <hemanthnarava2003@gmail.com>
2025-10-29 20:09:12 +0530

    Update Example_1. v

--- DAY_2/Example_1.v
+++ DAY_2/Example_1.v
@@ -1,1 +1,21 @@
+module pram_localparam #(
+    parameter WIDTH = 8      
+)(
+    input  wire [WIDTH-1:0] a,
+    input  wire [WIDTH-1:0] b,
+    output wire [WIDTH-1:0] sum
+);
+   
+    localparam LENGTH = 4;
 
+    assign sum = a + b;
+
+    initial begin
+        $display("=====================================");
+        $display("PARAMETERIZED ADDER SIMULATION");
+        $display("WIDTH  = %0d", WIDTH);
+        $display("LENGTH = %0d", LENGTH);
+        $display("=====================================");
+    end
+
+endmodule
