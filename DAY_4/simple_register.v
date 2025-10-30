333e2bc30935bd59cd34db4fa19397fb797a11fe
hemanthnarava2003-netizen <hemanthnarava2003@gmail.com>
2025-10-30 21:21:43 +0530

    Update simple_register.v

--- DAY_4/simple_register.v
+++ DAY_4/simple_register.v
@@ -1,1 +1,9 @@
-
+module simple_register (
+    input   clk,     
+    input   data_in,   
+    output   data_out   
+);
+    always @(posedge clk) begin
+        data_out <= data_in;  
+    end
+endmodule
