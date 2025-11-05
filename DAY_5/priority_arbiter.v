c8e7ca8a644206cd2a0bd97ca0942b39bccf98a3
hemanthnarava2003-netizen <hemanthnarava2003@gmail.com>
2025-11-05 12:48:32 +0530

    Update priority_arbiter.v

--- DAY_5/priority_arbiter.v
+++ DAY_5/priority_arbiter.v
@@ -1,1 +1,17 @@
-
+module priority_arbiter #(parameter N = 4) (
+    input  wire [N-1:0] req,
+    output reg  [N-1:0] grant
+);
+    integer i;
+    always @(*) begin
+        grant = 0; 
+        begin : prio_loop
+            for (i = 0; i < N; i = i + 1) begin
+                if (req[i]) begin
+                    grant = 1 << i;
+                    disable prio_loop;
+                end
+            end
+        end
+    end
+endmodule
