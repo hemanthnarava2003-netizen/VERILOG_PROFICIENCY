ae17cbbbe0e0677f50100c1687f8186a25e933ad
hemanthnarava2003-netizen <hemanthnarava2003@gmail.com>
2025-11-08 21:36:32 +0530

    Update tb_odd_sequence_generator.v

--- DAY_7/tb_odd_sequence_generator.v
+++ DAY_7/tb_odd_sequence_generator.v
@@ -1,1 +1,25 @@
+module tb_odd_sequence_generator;
 
+    reg clk;
+    reg rst;
+    wire [3:0] out;
+    odd_sequence_generator uut (.clk(clk),.rst(rst),.out(out));
+    always #5 clk = ~clk;
+    initial begin
+        $monitor($time, " ns | Output = %b (%0d)", out, out);
+    end
+    // Stimulus
+    initial begin
+        // Initialize clock and reset
+        clk = 0;
+        rst = 1;             // Apply reset
+        #10;                 // Hold reset for a while
+        rst = 0;             // Release reset
+        #200;
+        rst = 1;
+        #10;
+        rst = 0;
+        #100;
+        $stop;
+    end
+endmodule
